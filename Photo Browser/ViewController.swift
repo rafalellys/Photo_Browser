a4768a83287602493a252feb054dc0dcfeda56bf
Rafael Lellys <rafalellys@gmail.com>
2020-06-19 00:06:26 -0600

    Initial Commit

---
+++ Photo Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Browser
+//
+//  Created by Rafael Lellys on 2020-06-19.
+//  Copyright © 2020 Rafael Lellys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
